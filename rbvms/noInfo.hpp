3446247b7dd4c44df6bbcf1f90260b371ec08ed7
Ido Akkerman <I.Akkerman@tudelft.nl>
2024-08-28 14:49:39 +0200

    Build info tweaks2

---
+++ rbvms/noInfo.hpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <fstream>
+#include <sstream>
+std::istringstream buildInfo(R"~(
+------------------------------------
+No build information
+------------------------------------
+)~");
