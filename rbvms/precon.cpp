9f124c16f3a61422be910bcc7f0f509f6d969236
joostfitton <joostfitton4@gmail.com>
2024-12-04 09:59:56 +0100

    Update precon.cpp
    new hybrid precon, first block via HYPRE amg, then HYPRE ILU

--- rbvms/precon.cpp
+++ rbvms/precon.cpp
@@ -11,40 +11,40 @@ using namespace mfem;
 using namespace RBVMS;
 
 // Set the diagonal and off-diagonal operators
-void JacobianPreconditioner::SetOperator(const Operator &op)
+void ExtendedPreconditioner::SetOperator(const Operator &op)
 {
    BlockOperator *jacobian = (BlockOperator *) &op;
 
-   if (prec[0] == nullptr)
-   {
-      prec[0] = new HypreILU();//*Jpp);new HypreSmoother();//HypreILU()
-   }
-   if (prec[1] == nullptr)
-   {
-      HypreParMatrix* Jpp = dynamic_cast<HypreParMatrix*>(&jacobian->GetBlock(1,1));
-      HypreParMatrix *Jpp2 = const_cast<HypreParMatrix*>(Jpp);
-    //  prec[1] = new HypreSmoother();//*Jpp);
-      prec[1] = new HypreILU();//*Jpp);
-   }
-
    for (int i = 0; i < prec.Size(); ++i)
    {
-      prec[i]->SetOperator(jacobian->GetBlock(i,i));
+      if (!prec[i])
+      {
+         // Choose preconditioner type for each block
+         if (i == 0)
+         {
+            prec[i] = new HypreAMG(); // Use AMG for the first block
+         }
+         else
+         {
+            prec[i] = new HypreILU(); // Use ILU for other blocks
+         }
+      }
+
+      prec[i]->SetOperator(jacobian->GetBlock(i, i));
       SetDiagonalBlock(i, prec[i]);
 
-      for (int j = i+1; j < prec.Size(); ++j)
+      for (int j = i + 1; j < prec.Size(); ++j)
       {
-         SetBlock(j,i, const_cast<Operator*>(&jacobian->GetBlock(j,i)));
+         SetBlock(j, i, const_cast<Operator *>(&jacobian->GetBlock(j, i)));
       }
    }
 }
 
 // Destructor
-JacobianPreconditioner::~JacobianPreconditioner()
+ExtendedPreconditioner::~ExtendedPreconditioner()
 {
    for (int i = 0; i < prec.Size(); ++i)
    {
       delete prec[i];
    }
 }
-
