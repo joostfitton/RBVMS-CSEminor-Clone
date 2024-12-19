21a3954a038747e127a03fd76b89e68a043f37da
joostfitton <joostfitton4@gmail.com>
2024-12-19 11:34:44 +0100

    Update precon.cpp
    Edit precon to AMG for first block, ILU for rest

--- rbvms/precon.cpp
+++ rbvms/precon.cpp
@@ -13,34 +13,36 @@ using namespace RBVMS;
 // Set the diagonal and off-diagonal operators
 void JacobianPreconditioner::SetOperator(const Operator &op)
 {
-   BlockOperator *jacobian = (BlockOperator *) &op;
+    BlockOperator *jacobian = (BlockOperator *)&op;
 
-   if (prec[0] == nullptr)
-   {
-      prec[0] = new HypreILU();//*Jpp);new HypreSmoother();//HypreILU()
-   }
-   if (prec[1] == nullptr)
-   {
-      HypreParMatrix* Jpp = dynamic_cast<HypreParMatrix*>(&jacobian->GetBlock(1,1));
-      HypreParMatrix *Jpp2 = const_cast<HypreParMatrix*>(Jpp);
-    //  prec[1] = new HypreSmoother();//*Jpp);
-      prec[1] = new HypreILU();//*Jpp);
-   }
+    // Set up the preconditioner for the first block (AMG)
+    if (prec[0] == nullptr)
+    {
+        prec[0] = new HypreBoomerAMG(); // Use HYPRE AMG for the first block
+    }
 
-   for (int i = 0; i < prec.Size(); ++i)
-   {
-      prec[i]->SetOperator(jacobian->GetBlock(i,i));
-      SetDiagonalBlock(i, prec[i]);
+    // Set up the preconditioner for the remaining blocks (ILU)
+    for (int i = 1; i < prec.Size(); ++i)
+    {
+        if (prec[i] == nullptr)
+        {
+            prec[i] = new HypreILU(); // Use HYPRE ILU for other blocks
+        }
+    }
 
-      for (int j = i+1; j < prec.Size(); ++j)
-      {
-         SetBlock(j,i, const_cast<Operator*>(&jacobian->GetBlock(j,i)));
-      }
-   }
+    // Configure the blocks
+    for (int i = 0; i < prec.Size(); ++i)
+    {
+        prec[i]->SetOperator(jacobian->GetBlock(i, i));
+        SetDiagonalBlock(i, prec[i]);
+
+        for (int j = i + 1; j < prec.Size(); ++j)
+        {
+            SetBlock(j, i, const_cast<Operator *>(&jacobian->GetBlock(j, i)));
+        }
+    }
 }
 
-// Destructor
-JacobianPreconditioner::~JacobianPreconditioner()
 {
    for (int i = 0; i < prec.Size(); ++i)
    {
